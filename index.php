ffe5aae179b718e7fb90c2cc63a7c6a6fe01522b
Ryo Okada <63003268+ryookada123@users.noreply.github.com>
2020-04-01 12:55:18 -0400

    normalilze

--- index.php
+++ index.php
@@ -8,23 +8,7 @@
 	<link href="css/style.css" rel="stylesheet">
 </head>
 <body>
-
-	<div class="wrapper">
-		<header>
-			<nav>
-				<ul>
-					<li><a href="/">HOME</a></li>
-					<li><a href="/about/">ABOUT</a></li>
-				</ul>
-			</nav>
-		</header>
-    test2
 	<main>
-		<picture>
-		<source media="(min-width: 650px)" srcset="img/img_desktop@2x.jpg 2x, img/img_desktop.jpg 1x">
-		<img srcset="img/img_mobile@2x.jpg 2x" src="img/img_mobile.jpg">
-		</picture>
 	</main>
-	</div>
 </body>
 </html>

5aa4247563c1e7c0bd122eec161a9323fea79ef5
Ryo Okada <63003268+ryookada123@users.noreply.github.com>
2020-04-01 21:56:59 -0400

    commit

--- index.php
+++ index.php
@@ -9,7 +9,26 @@
 </head>
 <body>
 	<main>
-	text_second_branch
+		<div class="wrapper">
+			<div class="grid_wrap">
+				<div class="box">
+					<h2>Test1</h2>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt unde officia ea tempora mollitia doloremque vero illo cupiditate molestias quaerat.</p>
+				</div>
+				<div class="box">
+					<h2>Test2</h2>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt unde officia ea tempora mollitia doloremque vero illo cupiditate molestias quaerat.</p>
+				</div>
+				<div class="box">
+					<h2>Test3</h2>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt unde officia ea tempora mollitia doloremque vero illo cupiditate molestias quaerat.</p>
+				</div>
+				<div class="box">
+					<h2>Test5</h2>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt unde officia ea tempora mollitia doloremque vero illo cupiditate molestias quaerat.</p>
+				</div>
+			</div>
+		</div>
 	</main>
 </body>
 </html>
